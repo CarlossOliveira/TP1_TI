4312691d4f9e58e883424a87a592cb86cc402757
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-09 11:39:22 +0100

    Completed step 3 of TP1

--- source/main.py
+++ source/main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def create_plot(y, x, y_data, x_data, num_plot=1):
     plt.subplot(2,3,num_plot)
-    plt.scatter(x_data,y_data)
+    plt.scatter(x_data,y_data,color="darkmagenta")
     plt.xlabel(x)
     plt.ylabel(y)
     plt.title(f'{y} vs. {x}')
@@ -13,9 +13,9 @@ def create_plot(y, x, y_data, x_data, num_plot=1):
 
 
 if __name__ == "__main__":
-    data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file [1.a.]
-    matrix = data.values.tolist() # Convert the DataFrame to a matrix [1.b.]
-    parameters = data.columns.values.tolist() # Get the column names [1.c.]
+    data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file
+    matrix = data.values.tolist() # Convert the DataFrame to a matrix
+    parameters = data.columns.values.tolist() # Get the column names
     
     # Create plot window, give it a title and configure grid layout
     plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
@@ -38,4 +38,16 @@ if __name__ == "__main__":
     # MPG vs. Weight
     create_plot('MPG', 'Weight', data['MPG'], data['Weight'], 6)
 
+    # Convert all the data in data (type: DataFrame) to uint16 then extracts column values to a nparray to then convert it to a matrix as I've done to matrix
+    matrix_uint16 = (data.astype(np.uint16)).values
+    print(matrix_uint16)
+    
+    # Create an alphabet for matrix_uint16 and sort it
+    alphabet = []
+    for line in matrix_uint16:
+        for col in range(len(line)):
+            if (line[col] not in alphabet):
+                alphabet.append(line[col])
+    alphabet.sort()
+    
     plt.show()
