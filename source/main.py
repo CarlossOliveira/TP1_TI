ab7c27743c2d42c55f6ac363ff25ef30c784a603
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-09 09:44:12 +0100

    Complete exercise 1 of TP1

--- source/main.py
+++ source/main.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 
-data = pd.read_excel('data/CarDataSet.xlsx')
+data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file [1.a.]
 
-matrix = data.values.tolist()
-print(matrix)
-parameters = data.columns.values.tolist()
+matrix = data.values.tolist() # Convert the DataFrame to a matrix [1.b.]
+parameters = data.columns.values.tolist() # Get the column names [1.c.]
