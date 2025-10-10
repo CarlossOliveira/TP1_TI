a77f97577100ca4b21c60e8d692ffba9f8b298da
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-10 09:54:36 +0100

    Refine plot comments for clarity and consistency

--- source/main.py
+++ source/main.py
@@ -20,22 +20,22 @@ if __name__ == "__main__":
     # Create plot window, give it a title and configure grid layout
     plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
     
-    # MPG vs. Acceleration
+    # MPG vs. Acceleration Plot
     create_plot('MPG', 'Acceleration', data['MPG'], data['Acceleration'], 1)
     
-    # MPG vs. Cylinders
+    # MPG vs. Cylinders Plot
     create_plot('MPG', 'Cylinders', data['MPG'], data['Cylinders'], 2)
     
-    # MPG vs. Displacement
+    # MPG vs. Displacement Plot
     create_plot('MPG', 'Displacement', data['MPG'], data['Displacement'], 3)
     
-    # MPG vs. Horsepower
+    # MPG vs. Horsepower Plot
     create_plot('MPG', 'Horsepower', data['MPG'], data['Horsepower'], 4)
 
-    # MPG vs. Model Year
+    # MPG vs. Model Year Plot
     create_plot('MPG', 'Model Year', data['MPG'], data['ModelYear'], 5)
 
-    # MPG vs. Weight
+    # MPG vs. Weight Plot
     create_plot('MPG', 'Weight', data['MPG'], data['Weight'], 6)
 
     # Convert all the data in data (type: DataFrame) to uint16 then extracts column values to a nparray to then convert it to a matrix as I've done to matrix
@@ -49,5 +49,7 @@ if __name__ == "__main__":
             if (line[col] not in alphabet):
                 alphabet.append(line[col])
     alphabet.sort()
+
+    # Calculate the number of occurrences of each element of the alphabet in each variable of matrix_uint16s
     
     plt.show()
