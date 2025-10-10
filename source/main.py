2b44a5a0527b1972584cfab3e9f3877fed88f2a5
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-10 12:08:00 +0100

    Converted python code to python.numpy for better code efficiency in alphabet creation

--- source/main.py
+++ source/main.py
@@ -13,35 +13,27 @@ def create_plot(y, x, y_data, x_data, num_plot=1):
 
 def main():
     data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file
-    matrix = data.values.tolist() # Convert the DataFrame to a matrix
+    matrix = data.values # Convert the DataFrame to a matrix
     var_names = data.columns.values.tolist() # Get the column names
 
     # Create plot window, give it a title and configure grid layout
     plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
     
-    
+    # Create scatter plots for MPG vs each of the other variables
     for i in range(6):
         create_plot(var_names[6], var_names[i], data[var_names[6]], data[var_names[i]], i + 1)
     
+    # Convert all the data in matrix to uint16
+    matrix_uint16 = matrix.astype(np.uint16)
     
-    # Convert all the data in data (type: DataFrame) to uint16 then extracts column values to a nparray to then convert it to a matrix as I've done to matrix
-    matrix_uint16 = (data.astype(np.uint16)).values
-    print(matrix_uint16)
     # Create an alphabet for matrix_uint16 and sort it
-    alphabet = []
-    for line in matrix_uint16:
-        for col in range(len(line)):
-            if (line[col] not in alphabet):
-                alphabet.append(line[col])
-    print("-----------------\n", alphabet)
+    alphabet = np.array([], dtype=np.uint16)
+    alphabet = np.unique(matrix_uint16)
+    alphabet = np.sort(alphabet)
     
-    # if (matrix_uint16[:][:] not in alphabet):
-    #     alphabet.append(matrix_uint16[:][:])
-    alphabet.sort()
-    print("-----------------\n", alphabet)
     # Calculate the number of occurrences of each element of the alphabet in each variable of matrix_uint16s
     
-    plt.show()
+    #plt.show()
 
 
 if __name__ == "__main__":
