940c85933942f2471c61a0aa117b2c7f15e9e8f2
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-10 23:37:06 +0100

    SOLVED: Issue with calculating the number of occurrences of each alphabet element in each column

--- source/main.py
+++ source/main.py
@@ -30,14 +30,15 @@ def main():
     alphabet = np.array([], dtype=np.uint16)
     alphabet = np.unique(matrix_uint16)
     
-    # Calculate the number of occurrences of each element of the alphabet in each variable (column) [ERRO]
-    total_occurrences = np.zeros((len(alphabet), matrix_uint16.shape[1]), dtype=np.uint16)
-    for num in alphabet:
-        occurrences=0
-        arr=np.where(matrix_uint16[:, None] == num, occurrences + 1, occurrences)
-        total_occurrences += arr.astype(np.uint16)
+    # Calculate the number of occurrences of each element of the alphabet in each variable (column)
+    total_occurrences = np.zeros((len(var_names), len(alphabet)), dtype=np.uint16) # Creates a 2 axis array (matrix) filled with 0s with the number of rows equal to the number of variables (columns in the original matrix) and the number of columns equal to the number of unique elements in the alphabet. This matrix will be used to store the count of occurrences of each element of the alphabet in each variable (column).
+    for col in range(len(var_names)):
+        for symbol in range(len(alphabet)):
+            occurrences = matrix_uint16[:, col] == alphabet[symbol] # Creates a boolean array where the symbol is found in the column. Example case: We are searching for the number of occurences of the symbol 5 in column 0 (column 0 = [5,2,5,6,0,89,5]). The occurrences array will be [True, False, True, False, False, False, True] where True indicates the presence of the symbol.
+            total_occurrences[col, symbol] = np.sum(occurrences) # Sum the boolean array to get the number of occurrences of the symbol. NOTE: It's important to remember that in Python, True is equivalent to 1 and False is equivalent to 0. So, summing the boolean array gives the count of True values, which corresponds to the number of occurrences of the symbol in the column.
+
     print("Alphabet:\n", alphabet)
-    print("Total occurrences of each element of the alphabet in each variable (column):\n", total_occurrences)
+    print("Total occurrences of each element of the alphabet in each variable (column):\n", total_occurrences.tolist())
     
     plt.show()
 
