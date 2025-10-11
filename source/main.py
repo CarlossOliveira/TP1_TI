7d6d6f4b72d47e2746410770be0be92ae02b3d33
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-11 12:12:50 +0100

    Remove unnecessary initialization of alphabet and debug print statements in main function

--- source/main.py
+++ source/main.py
@@ -27,7 +27,6 @@ def main():
     matrix_uint16 = matrix.astype(np.uint16)
     
     # Create an alphabet for matrix_uint16
-    alphabet = np.array([], dtype=np.uint16)
     alphabet = np.unique(matrix_uint16)
     
     # Calculate the number of occurrences of each element of the alphabet in each variable (column)
@@ -36,9 +35,6 @@ def main():
         for symbol in range(len(alphabet)):
             occurrences = matrix_uint16[:, col] == alphabet[symbol] # Creates a boolean array where the symbol is found in the column. Example case: We are searching for the number of occurences of the symbol 5 in column 0 (column 0 = [5,2,5,6,0,89,5]). The occurrences array will be [True, False, True, False, False, False, True] where True indicates the presence of the symbol.
             total_occurrences[col, symbol] = np.sum(occurrences) # Sum the boolean array to get the number of occurrences of the symbol. NOTE: It's important to remember that in Python, True is equivalent to 1 and False is equivalent to 0. So, summing the boolean array gives the count of True values, which corresponds to the number of occurrences of the symbol in the column.
-
-    print("Alphabet:\n", alphabet)
-    print("Total occurrences of each element of the alphabet in each variable (column):\n", total_occurrences.tolist())
     
     plt.show()
 
