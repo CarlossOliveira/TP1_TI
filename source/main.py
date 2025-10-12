6235514abaf944ac7f72238a18601614197d8149
miguelgaspar <miguelcunhagaspar@gmail.com>
2025-10-12 16:15:16 +0100

    Refactor bar graph creation and remove debug print statements in main function

--- source/main.py
+++ source/main.py
@@ -11,6 +11,14 @@ def create_plot(y, x, y_data, x_data, num_plot=1):
     plt.title(f'{y} vs. {x}')
     plt.autoscale()
 
+def create_bar_graph(x, x_data, y_data):
+    plt.figure(layout="tight", num=f'Número de ocorrências representado em gráfico de barras - {x}')
+    positions = np.arange(len(x_data)) # Create an array with positions for each bar
+    plt.bar(positions, y_data, color='red', align='center') # Create the bar graph
+    plt.xticks(positions, x_data, fontsize=6) 
+    plt.xlabel(x)
+    plt.ylabel("Count")
+
 def main():
     data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file
     matrix = data.values # Convert the DataFrame to a matrix
@@ -36,9 +44,10 @@ def main():
             occurrences = matrix_uint16[:, col] == alphabet[symbol] # Creates a boolean array where the symbol is found in the column. Example case: We are searching for the number of occurences of the symbol 5 in column 0 (column 0 = [5,2,5,6,0,89,5]). The occurrences array will be [True, False, True, False, False, False, True] where True indicates the presence of the symbol.
             total_occurrences[col, symbol] = np.sum(occurrences) # Sum the boolean array to get the number of occurrences of the symbol. NOTE: It's important to remember that in Python, True is equivalent to 1 and False is equivalent to 0. So, summing the boolean array gives the count of True values, which corresponds to the number of occurrences of the symbol in the column.
 
-    print("Alphabet:\n", alphabet)
-    print("Total occurrences of each element of the alphabet in each variable (column):\n", total_occurrences.tolist())
-    
+    # Create bar graphs for each variable
+    for var in range(len(var_names)):
+        indice_nonzero = total_occurrences[var].nonzero()[0] 
+        create_bar_graph(var_names[var], alphabet[indice_nonzero], total_occurrences[var][indice_nonzero])
     plt.show()
 
 if __name__ == "__main__":
