c2af0a3461216bdece01b1eaf34352f59ef8a82b
bernamachado06-sketch <bernamachado06@gmail.com>
2025-10-16 14:16:47 +0100

    Last version of the code

--- source/main.py
+++ source/main.py
@@ -3,62 +3,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def create_plot(y, x, y_data, x_data, num_plot=1):
-    plt.subplot(3,2,num_plot)
-    plt.scatter(x_data,y_data,color="m")
+def create_plot(x, y, x_data, y_data, num_plot, comp):
+    plt.subplot(comp//2,2,num_plot) #grafico 3 linhas 2 colunas
+    plt.scatter(x_data,y_data,color="m") #O eixo x recebe os valores de x_data, O eixo y recebe os valores de y_data.
     plt.xlabel(x)
     plt.ylabel(y)
     plt.title(f'{y} vs. {x}')
-    plt.autoscale()
+    plt.autoscale() #ajusta automaticamente os limites dos eixos (X e Y) com base nos dados que foram desenhados no gráfico.    
 
-def numberOfOccurrences(alphabet, matrix_uint16):
-    list_alphabet = alphabet.tolist()
-    occurrences_list = [0] * len(list_alphabet)  # Inicializa com zeros
 
-    for symbol in range(len(list_alphabet)):
-        occurrences = 0
-        for row in range(len(matrix_uint16)):
-            for col in range(len(matrix_uint16[0])):
-                if matrix_uint16[row][col] == list_alphabet[symbol]:
-                    occurrences += 1
-        occurrences_list[symbol] = occurrences  # Substitui o valor na posição correta
+def create_plot_bar(alphabet, numberOccurrences, var_names):
         
-    occurrences_alphabet = np.array(occurrences_list, dtype=np.uint16)
+    plt.figure(layout = "tight", num = f"Nuemro de - {var_names}")
+    plt.bar(alphabet.astype("str"), numberOccurrences, color='red', align= "center")
+    plt.title('Distribuição ' + var_names)
+    plt.xlabel(var_names)
+    plt.ylabel("Count")
+    plt.tight_layout()
+    plt.autoscale()
+    plt.show()
 
-    return occurrences_alphabet 
-
+    
 def main():
     data = pd.read_excel('data/CarDataSet.xlsx') # Read the Excel file
-    matrix = data.values # Convert the DataFrame to a matrix
+    matrix = data.values # Convert the DataFrame to a matrix, funcao de pandas
     var_names = data.columns.values.tolist() # Get the column names
-
+    
     # Create plot window, give it a title and configure grid layout
-    plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
+    
+    plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(10,6)) 
+    
     
     # Create scatter plots for MPG vs each of the other variables
-    for i in range(6):
-        create_plot(var_names[6], var_names[i], data[var_names[6]], data[var_names[i]], i + 1)
+    
+    comp_var = len(var_names) - 1
+    
+    for i in range(comp_var):
+        create_plot(var_names[i], var_names[comp_var], data[var_names[i]], data[var_names[comp_var]], i + 1, comp_var)
+    #plt.show()
     
     
     # 3)
+    
     # Convert all the data in matrix to uint16
     matrix_uint16 = matrix.astype(np.uint16) 
     
-    # Create an alphabet for matrix_uint16
-    alphabet = np.array([], dtype=np.uint16)
-    alphabet = np.unique(matrix_uint16)
-    
-    
-    #Versao Bernardo 3)
-    matrix_uint16 = matrix.astype(np.uint16)
-    alphabet = np.unique(matrix_uint16)
-
-        
+            
     # 4)
-    print("Alphabet:\n", alphabet)
-    print("Total occurrences of each element of the alphabet in each variable (column):\n", numberOfOccurrences(alphabet, matrix_uint16))
     
-    #plt.show()
+    alphabet = [None] * len(var_names)
+    numberOccurrences = [None] * len(var_names)
+
+    for i in range(len(var_names)):
+        unique_vals, counts = np.unique(matrix_uint16[:, i], return_counts=True)
+        alphabet[i] = unique_vals.astype(np.uint16)
+        numberOccurrences[i] = counts.astype(np.uint16)
+    
+    
+    # 5)
+    
+    for i in range(comp_var):    
+        create_plot_bar(alphabet[i], numberOccurrences[i], var_names[i])
+
+    #6 
+    
+    # np.where(minimo intrevalo e max intrevalo, TRUE acontecimento, False acontecimento) depois devol usar no exercicio 6
+
 
 
 if __name__ == "__main__":
