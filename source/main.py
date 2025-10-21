a4687b37237f8cbc351f79009006b0a82747b127
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-21 10:32:02 +0100

    Refactor data processing in main function to streamline unique value extraction and symbol counting for bar chart creation

--- source/main.py
+++ source/main.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-import time as t
 
 
 def create_plot(y, x, y_data, x_data, num_plot=1):
@@ -32,41 +31,32 @@ def main():
     plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
     
     # Create scatter plots for MPG vs each of the other variables
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Creating scatter plots...")
     for i in range(6):
         create_plot(var_names[6], var_names[i], data[var_names[6]], data[var_names[i]], i + 1)
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Scatter plots created successfully.")
     
     # Convert all the data in matrix to uint16
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Converting data to uint16...")
     matrix_uint16 = matrix.astype(np.uint16)
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Data converted to uint16 successfully.")
     
-    # Create an alphabet for matrix_uint16
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Creating alphabet...")
-    alphabet = np.unique(matrix_uint16)
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Alphabet created successfully.")
-    
-    # Calculate the number of occurrences of each element of the alphabet in each variable (column)
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Creating total occurrences matrix...")
-    total_occurrences = np.zeros((len(var_names), len(alphabet)), dtype=np.uint16) # Creates a 2 axis array (matrix) filled with 0s with the number of rows equal to the number of variables (columns in the original matrix) and the number of columns equal to the number of unique elements in the alphabet. This matrix will be used to store the count of occurrences of each element of the alphabet in each variable (column).
+    # Create alphabet and symbol count arrays for each variable
+    alphabet = [None] * len(var_names)
+    symbol_count = [None] * len(var_names)
     for col in range(len(var_names)):
-        for symbol in range(len(alphabet)):
-            occurrences = matrix_uint16[:, col] == alphabet[symbol] # Creates a boolean array where the symbol is found in the column. Example case: We are searching for the number of occurences of the symbol 5 in column 0 (column 0 = [5, 2, 5, 6, 0, 89, 5]). The occurrences array will be [True, False, True, False, False, False, True] where True indicates the presence of the symbol.
-            total_occurrences[col, symbol] = np.sum(occurrences) # Sum the boolean array to get the number of occurrences of the symbol. NOTE: It's important to remember that in Python, True is equivalent to 1 and False is equivalent to 0. So, summing the boolean array gives the count of True values, which corresponds to the number of occurrences of the symbol in the column.
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Total occurrences matrix created successfully.")
+        # Get unique values and their counts for each column
+        unique_vals, counts = np.unique(matrix_uint16[:, col], return_counts=True)
+        
+        # Create an alphabet for matrix_uint16
+        alphabet[col] = unique_vals
+        
+        # Create a symbol count array initialized to zeros
+        symbol_count[col] = np.zeros(len(alphabet[col]), dtype=np.uint16)
+        symbol_count[col] = counts
+        
+        # Criar gráficos de barras para cada variável, filtrando ocorrências zero
+        indices_nonzero = np.nonzero(symbol_count[col])[0]
+        create_bar_chart(var_names[col], symbol_count[col][indices_nonzero], alphabet[col][indices_nonzero])
 
-    # Create bar charts for each variable using the total_occurrences matrix and filters out the zero occurrences
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Creating bar charts...")
-    for var in range(len(var_names)):
-        indices_nonzero = total_occurrences[var].nonzero()[0]
-        create_bar_chart(var_names[var], total_occurrences[var][indices_nonzero], alphabet[indices_nonzero])
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] Bar charts created successfully.")
-
-    plt.show()
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] --> Program ended successfully.")
+        plt.show()
 
 
 if __name__ == "__main__":
-    print(f"[{t.strftime('%Y-%m-%d %H:%M:%S')}] --> Program started.")
     main()
