239df4e345dbdfae9da7572c61094df48de028d9
Carlos Miguel Oliveira <carlosoliveira2006@icloud.com>
2025-10-22 17:06:40 +0100

    Coppied Miguel's Branch

--- source/main.py
+++ source/main.py
@@ -1,62 +1,127 @@
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
+import huffmancodec as huffc
 
-
-def create_plot(y, x, y_data, x_data, num_plot=1):
-    plt.subplot(3,2,num_plot)
-    plt.scatter(x_data,y_data,color="m")
+# Function to create plots
+def create_plot(x, y, x_data, y_data, num_plot, comp):
+    plt.subplot(comp//2,2,num_plot) #grafico 3 linhas 2 colunas
+    plt.scatter(x_data,y_data,color="m") #O eixo x recebe os valores de x_data, O eixo y recebe os valores de y_data.
     plt.xlabel(x)
     plt.ylabel(y)
     plt.title(f'{y} vs. {x}')
-    plt.autoscale()
+    plt.autoscale() #ajusta automaticamente os limites dos eixos (X e Y) com base nos dados que foram desenhados no gráfico. 
 
-
-def create_bar_chart(x, y_data, x_data):
-    name = f"Número de ocorrências representado em gráfico de barras - {x}"
-    positions = np.arange(len(x_data))  # The label locations [0, 1, 2, ..., len(x_data)-1]
-    plt.figure(layout="tight", num=name)
-    plt.bar(positions, y_data, color="red", align="center") # NOTE: The positions array is used to position the bars on the x-axis based on the number of unique elements in x_data.
-    plt.xticks(positions, x_data) # The xticks function is used to set the labels on the x-axis to the unique elements in x_data and to position them correctly under the bars.
+# Function to plot bars
+def create_plot_bar(alphabet, numberOccurrences, var_names):
+    plt.figure(layout = "tight", num = f"Nuemro de - {var_names}")
+    plt.bar(alphabet.astype('str'), numberOccurrences, color='red', align= "center")
+    plt.title('Distribuição ' + var_names)
+    plt.xlabel(var_names)
     plt.ylabel("Count")
-    plt.xlabel(x)
+    plt.xticks(rotation = 90)
+
+    plt.show()
+
+# Function to calculate number of occurrences
+def extractAlphabetCounts(matrix_uint16, var_names):
+    alphabet = [None] * len(var_names)
+    numberOccurrences = [None] * len(var_names)
+    
+    for i in range (len(var_names)):
+        unique_vals, counts = np.unique(matrix_uint16[:, i], return_counts=True)
+        alphabet[i] = unique_vals.astype(np.uint16)
+        numberOccurrences[i] = counts.astype(np.uint16)
+
+    return alphabet, numberOccurrences
+
+# Function of binning
+def binning(matrix, step, indice):
+    colunaVar = matrix[:, indice].copy()
+    valorMax = int(np.max(colunaVar))
+
+    for i in range(0, valorMax + 1, step):
+        intervalo = (colunaVar >= i) & (colunaVar < i + step)
+        if not np.any(intervalo):
+            continue
+        # Use bincount to find the most frequent value in the interval
+        values_in_interval = colunaVar[intervalo]
+        if len(values_in_interval) == 0:
+            continue
+        unique_values, counts = np.unique(values_in_interval, return_counts=True)
+        replacement_value = unique_values[np.argmax(counts)]
+
+        colunaVar = np.where(intervalo, replacement_value, colunaVar)
+
+    matrix[:, indice] = colunaVar
+    return matrix
+
+# Functoin to calculate entrophy
+def calcularEntropia(numberOccurrences):
+    p = numberOccurrences / np.sum(numberOccurrences)
+    H = -np.sum(p * np.log2(p))
+    return H
+
+# Huffman
+def huffman(numberOccurrences, list_num):
+    codec = huffc.HuffmanCodec.from_data(list_num)
+    symbols, lenghts = codec.get_code_len()
+
 
 
 def main():
-    data = pd.read_excel(r"data/CarDataSet.xlsx") # Read the Excel file
-    matrix = data.values # Convert the DataFrame to a matrix
+    # Ex1: ler dados
+    data = pd.read_excel('/Users/miguel/Desktop/GitHub/TP1_TI/data/CarDataset.xlsx')
+    matrix = data.values # Convert the DataFrame to a matrix, funcao de pandas
     var_names = data.columns.values.tolist() # Get the column names
 
-    # Create plot window, give it a title and configure grid layout for the 6 scatter plots
-    plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(15,10))
+    # Ex2: Create scatter plots for MPG vs each of the other variables
+    plt.figure(layout="tight", num="relação entre MPG e as diferentes variáveis (características do carro)", figsize=(10,6))
+    comp_var = len(var_names) - 1
+    for i in range(comp_var):
+        create_plot(var_names[i], var_names[comp_var], data[var_names[i]], data[var_names[comp_var]], i + 1, comp_var)
+    plt.show()
     
-    # Create scatter plots for MPG vs each of the other variables
-    for i in range(6):
-        create_plot(var_names[6], var_names[i], data[var_names[6]], data[var_names[i]], i + 1)
-    
-    # Convert all the data in matrix to uint16
-    matrix_uint16 = matrix.astype(np.uint16)
-    
-    # Create alphabet and symbol count arrays for each variable
-    alphabet = [None] * len(var_names)
-    symbol_count = [None] * len(var_names)
-    for col in range(len(var_names)):
-        # Get unique values and their counts for each column
-        unique_vals, counts = np.unique(matrix_uint16[:, col], return_counts=True)
-        
-        # Create an alphabet for matrix_uint16
-        alphabet[col] = unique_vals
-        
-        # Create a symbol count array initialized to zeros
-        symbol_count[col] = np.zeros(len(alphabet[col]), dtype=np.uint16)
-        symbol_count[col] = counts
-        
-        # Criar gráficos de barras para cada variável, filtrando ocorrências zero
-        indices_nonzero = np.nonzero(symbol_count[col])[0]
-        create_bar_chart(var_names[col], symbol_count[col][indices_nonzero], alphabet[col][indices_nonzero])
+    # Ex3: Convert all the data in matrix to uint16
+    matrix_uint16 = matrix.astype(np.uint16) 
+           
+    # Ex4: Calculate occurrences
+    alphabet, numberOccurrences = extractAlphabetCounts(matrix_uint16, var_names)
 
-        plt.show()
+    # Ex5: Plot bars
+    for i in range (comp_var):
+        create_plot_bar(alphabet[i], numberOccurrences[i], var_names[i])
 
+    # Ex6: apply binning to some variables
+    columnsVar = ['Displacement', 'Horsepower', 'Weight']
+    step_sizes = [5, 5, 40]
+    binned_data = matrix_uint16.copy() 
+    var_names_arr = np.array(var_names)
+    updated_counts = {}
+    
+    for i in range(len(columnsVar)):
+        var = columnsVar[i]
+        step = step_sizes[i]
+        index = np.where(var_names_arr == var)[0][0]
+
+        binned_data = binning(binned_data, step, index)
+
+        # Visualize the distribution after binning
+        unique_vals, counts = np.unique(binned_data[:, index], return_counts=True)
+        updated_counts[var] = counts
+        create_plot_bar(unique_vals, counts, var)
+
+    # Ex7: calculate entrophy 
+    for i in range (len(var_names)):
+        var = var_names[i]
+        
+        if var in updated_counts:
+            counts = updated_counts[var]
+        else:
+            counts = numberOccurrences[i]
+
+        entropia = calcularEntropia(counts)
+        print(f"H{var[:3]}= {entropia}")
 
 if __name__ == "__main__":
     main()
